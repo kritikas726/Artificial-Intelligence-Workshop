5d5c9c86a70a67cc6ef3629cbfb739c9f8fa49af
kritikas726 <52852774+kritikas726@users.noreply.github.com>
2021-09-14 15:31:46 +0530

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+E={1,2,3,4,5,6}
+N={5,6,7,8,9,0}
+Union=E.union(N)
+print(Union)
+
+
+# In[2]:
+
+
+Intersection=E.intersection(N)
+print(Intersection)
+
+
+# In[3]:
+
+
+Difference=E.difference(N)
+print(Difference)
+
+
+# In[4]:
+
+
+Difference1=N.difference(E)
+print(Difference1)
+
+
+# In[5]:
+
+
+Symmetric_Difference=E.symmetric_difference(N)
+print(Symmetric_Difference)
+
+
+# In[ ]:
+
+
+
+
